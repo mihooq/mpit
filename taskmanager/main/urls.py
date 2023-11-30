05efc55b2fdb3a657f9af142aa1ab92752772301
mihooq <margos14032008@gmail.com>
2023-11-30 18:44:38 +0300

    nothing 3

--- taskmanager/main/urls.py
+++ taskmanager/main/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('what_is', views.what_is),
+    path('wallet', views.wallet),
+    path('categories', views.categories),
+    path('tasks', views.tasks),
+    path('partners', views.partners),
 ]
--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -3,4 +3,23 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h4>MPIT</h4>")
+    return render(request, 'main/index.html')
+
+
+def what_is(request):
+    return render(request, 'main/what_is.html')
+
+
+def wallet(request):
+    return render(request, 'main/wallet.html')
+
+
+def categories(request):
+    return render(request, 'main/categories.html')
+
+
+def tasks(request):
+    return render(request, 'main/tasks.html')
+
+def partners(request):
+    return render(request, 'main/partners.html')
