0ed435ffea63bb9d1fb03e0d2b7592c54b0094d8
mihooq <margos14032008@gmail.com>
2023-12-01 10:20:44 +0300

    fy

--- taskmanager/main/models.py
+++ taskmanager/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
+class Logins(models.Model):
+    log = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.log
+
+class Passwords(models.Model):
+    logins = models.ForeignKey(Logins, on_delete=models.CASCADE)
+    pword = models.CharField(max_length=300)
+    complete = models.BooleanField()
+
+    def __str__(self):
+        return self.pword
+
 # Create your models here.
